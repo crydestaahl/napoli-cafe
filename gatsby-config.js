f42ce88ff4b7b8da3d1f69fcff46a33d5e72f1d8
crydestaahl <kristoffer@oholics.com>
2019-08-25 21:51:59 +0200

    trying...

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/blog/`,
+        path: `${__dirname}/blog`,
         name: `blog`,
       },
     },
