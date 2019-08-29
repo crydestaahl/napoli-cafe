3375e9efd4945b574325b277b61d25a05114eb20
crydestaahl <kristoffer@oholics.com>
2019-08-29 18:28:50 +0200

    changed excerpt...

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -91,7 +91,7 @@ export const pageQuery = graphql`
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
-      excerpt(pruneLength: 1600)
+      excerpt(pruneLength: 200)
       html
       frontmatter {
         title
