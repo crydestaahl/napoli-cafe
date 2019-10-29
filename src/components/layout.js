b03d2f884542da5a14781ab9ccc69123a484cd3d
crydestaahl <kristoffer@oholics.com>
2019-10-29 21:29:00 +0100

    corrected spelling.

--- src/components/layout.js
+++ src/components/layout.js
@@ -552,7 +552,7 @@ class Layout extends React.Component {
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>{` `} 
              edited by acidCode.
-            Got something on your mind? Contact <a href="mailto:dimmi@napoli-cafe.com">Dimmie</a>. 
+            Got something on your mind? Contact <a href="mailto:dimmi@napoli-cafe.com">Dimmi</a>. 
           </footer>
         </Fade>  
       </div>
